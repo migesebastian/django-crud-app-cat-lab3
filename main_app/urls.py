d557ffcac9be56cbf226ae4e19fa712dd09201ab
mmarrone97 <mmarrone97@gmail.com>
2024-08-21 09:05:15 -0400

    completed' '

---
+++ main_app/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from . import views # Import views to connect routes to view functions
+
+urlpatterns = [
+    path('', views.Home.as_view(), name='home'),
+    path('about/', views.about, name='about'),
+    path('dogs/', views.dog_index, name='dog-index'),
+    path('dogs/<int:dog_id>/', views.dog_detail, name='dog-detail'),
+    path('dogs/create/', views.DogCreate.as_view(), name='dog-create'),
+    path('dogs/<int:pk>/update/', views.DogUpdate.as_view(), name='dog-update'),
+    path('dogs/<int:pk>/delete/', views.DogDelete.as_view(), name='dog-delete'),
+    path('dogs/<int:dog_id>/add-feeding/', views.add_feeding, name='add-feeding'),
+    path('toys/create/', views.ToyCreate.as_view(), name='toy-create'),
+    path('toys/<int:pk>/', views.ToyDetail.as_view(), name='toy-detail'),
+    path('toys/', views.ToyList.as_view(), name='toy-index'),
+    path('toys/<int:pk>/update/', views.ToyUpdate.as_view(), name='toy-update'),
+    path('toys/<int:pk>/delete/', views.ToyDelete.as_view(), name='toy-delete'),
+    path('dogs/<int:dog_id>/associate-toy/<int:toy_id>/', views.associate_toy, name='associate-toy'),
+    path('dogs/<int:dog_id>/remove-toy/<int:toy_id>/', views.remove_toy, name='remove-toy'),
+    path('accounts/signup/', views.signup, name='signup'),
+]
---
+++ main_app/views.py
@@ -0,0 +1,113 @@
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Dog, Toy
+from .forms import FeedingForm
+
+# Create your views here.
+
+# Define the home view
+class Home(LoginView):
+  template_name = 'home.html'
+
+def about(request):
+  return render(request, 'about.html')
+
+@login_required
+def dog_index(request):
+  dogs = Dog.objects.filter(user=request.user)
+  return render(request, 'dogs/index.html', { 'dogs': dogs })
+
+@login_required
+def dog_detail(request, dog_id):
+  dog = Dog.objects.get(id=dog_id)
+  toys_dog_doesnt_have = Toy.objects.exclude(id__in = dog.toys.all().values_list('id'))
+  feeding_form = FeedingForm()
+  return render(request, 'dogs/detail.html', {'dog': dog, 'feeding_form': feeding_form, 'toys': toys_dog_doesnt_have })
+
+class DogCreate(LoginRequiredMixin, CreateView):
+  model = Dog
+  fields = ['name', 'breed', 'description', 'age']
+
+  def form_valid(self, form):
+    form.instance.user = self.request.user
+    return super().form_valid(form)
+
+class DogUpdate(LoginRequiredMixin, UpdateView):
+  model = Dog
+  fields = ['breed', 'description', 'age']
+
+class DogDelete(LoginRequiredMixin, DeleteView):
+  model = Dog
+  success_url = '/dogs/'
+
+@login_required
+def add_feeding(request, dog_id):
+  form = FeedingForm(request.POST)
+
+  if form.is_valid():
+    new_feeding = form.save(commit=False)
+    new_feeding.dog_id = dog_id
+    new_feeding.save()
+  return redirect('dog-detail', dog_id=dog_id)
+
+class ToyCreate(LoginRequiredMixin, CreateView):
+  model = Toy
+  fields = '__all__'
+
+
+class ToyList(LoginRequiredMixin, ListView):
+  model = Toy
+
+class ToyDetail(LoginRequiredMixin, DetailView):
+  model = Toy
+
+class ToyUpdate(LoginRequiredMixin, UpdateView):
+  model = Toy
+  fields = ['name', 'color']
+
+class ToyDelete(LoginRequiredMixin, DeleteView):
+  model = Toy
+  success_url = '/toys/'
+
+@login_required
+def associate_toy(request, dog_id, toy_id):
+  Dog.objects.get(id=dog_id).toys.add(toy_id)
+  return redirect('dog-detail', dog_id=dog_id)
+
+@login_required
+def remove_toy(request, dog_id, toy_id):
+  dog = Dog.objects.get(id=dog_id)
+  toy = Toy.objects.get(id=toy_id)
+  dog.toys.remove(toy)
+  return redirect('dog-detail', dog_id=dog.id)
+
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        # This is how to create a 'user' form object
+        # that includes the data from the browser
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            # This will add the user to the database
+            user = form.save()
+            # This is how we log a user in
+            login(request, user)
+            return redirect('dog-index')
+        else:
+            error_message = 'Invalid sign up - try again'
+    # A bad POST or a GET request, so render signup.html with an empty form
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'signup.html', context)
+    # Same as: 
+    # return render(
+    #     request, 
+    #     'signup.html',
+    #     {'form': form, 'error_message': error_message}
+    # )
